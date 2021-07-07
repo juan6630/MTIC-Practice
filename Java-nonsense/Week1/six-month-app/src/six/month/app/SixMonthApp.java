0cf65338b008ebaa6c882f1a6f6378ec3b8547e2
Juan Ospina <juanospina6630@gmail.com>
2021-07-06 20:19:40 -0500

    Pre-reto week 1

---
+++ Java-nonsense/Week1/six-month-app/src/six/month/app/SixMonthApp.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package six.month.app;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author juan6
+ */
+public class SixMonthApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner r = new Scanner(System.in);
+
+        int sucursales, meses;
+        
+        System.out.println("Ingrese la cantidad de sucursales:  ");
+        sucursales = r.nextInt();
+        System.out.println("Ingrese la cantidad de meses:   ");
+        meses = r.nextInt();
+        
+        float[][] matrix = new float[sucursales][meses];
+        float[] total_personas = new float[sucursales];
+        float[] solution1 = new float[2];
+        float[] total_mes = new float[meses];
+        float[] solution2 = new float[2];
+        
+        for(int i=0; i<sucursales; i++){
+            for(int j=0; j<meses; j++){
+                System.out.printf("Ingrese el dato de la sucursal %d en el mes %d:  ", i+1,j+1);
+                matrix[i][j] = r.nextFloat();
+            }
+        }
+        
+        //Sucursal con mayor ingreso de personas P1
+        for(int k=0; k<sucursales; k++){
+            total_personas[k] = 0;
+            for(int x=0; x<meses; x++){
+                total_personas[k] += matrix[k][x];
+            }
+//            System.out.printf("Total de la sucursal %d es %1.1f%n", k+1,total_personas[k]);
+        }
+        solution1[0] = 0;
+        solution1[1] = total_personas[0];
+        for(int i=1; i<sucursales;i++){
+            if(solution1[1] < total_personas[i]){
+                solution1[0] = i;
+                solution1[1] = total_personas[i];
+            }
+        }
+        System.out.printf("La sucursal con mayor ingreso de personas fue %5.0f%n con %5.1f%n comensales", solution1[0]+1,solution1[1]);
+        
+        //Mes con menor cantidad de comensales P2
+        for(int i=0;i<meses;i++){
+            total_mes[i] = 0;
+            for(int j=0;j<sucursales;j++){
+                total_mes[i] += matrix[j][i];
+            }
+        }
+        solution2[0] = 0;
+        solution2[1] = total_mes[0];
+        for(int k=1;k<meses;k++){
+            if(solution2[1] > total_mes[k]){
+                solution2[0] = k;
+                solution2[1] = total_mes[k];
+            }
+        }
+        //TODO número del mes a palabra
+        System.out.printf("El mes con menor cantidad de comensales fue %1.0f%n con %5.1f%n comensales", solution2[0]+1,solution2[1]);
+        
+    }
+    
+}
